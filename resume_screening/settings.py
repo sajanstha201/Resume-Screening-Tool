5a6f05e326f6b52b60be422646b22647aa7b916a
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-05-17 11:44:32 +0530

    added some templates

--- resume_screening/settings.py
+++ resume_screening/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "resume_screening.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
