ef484f4524b35d261db47ca546b69619a40db665
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-06-01 22:03:07 +0545

    added the download feature in rating section so that the user can donwload the csv file also

--- resume_screening/views.py
+++ resume_screening/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
+from django.http import HttpResponse
+from io import BytesIO
 import json
 from .ml_model.main import rating
 def home(request):
@@ -17,8 +19,17 @@ def rating_response(request):
     data_dict = json.loads(data)
     job_description=data_dict['job_description']
     resume_detail=data_dict['resume_detail']
-    rating_score,pdf_score=rating(jb_description=job_description,resume_dict=resume_detail)
-    return JsonResponse(rating_score)
+    rating_score,csv_score=rating(jb_description=job_description,resume_dict=resume_detail)
+    request_format=request.GET.get('format','json')
+    if(request_format=='csv'):
+        buffer=BytesIO()
+        csv_score.to_csv(buffer,index=False)
+        buffer.seek(0)
+        response=HttpResponse(buffer,content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="dataframe.csv"'
+        return response
+    else:
+        return JsonResponse(rating_score)
 
 def token_response(request):
     return JsonResponse({'token':get_token(request)})
