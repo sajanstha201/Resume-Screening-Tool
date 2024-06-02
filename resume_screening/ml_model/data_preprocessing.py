83101f1f470e8f4e183b33524646c8f28708cfff
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-06-02 08:06:30 +0545

    modified some socring factor and added a function that will remove the common words in job description while finding the critical keywords

--- resume_screening/ml_model/data_preprocessing.py
+++ resume_screening/ml_model/data_preprocessing.py
@@ -1,9 +1,19 @@
 import nltk
+from pathlib import Path
+import os
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+def remove_common_words(lemmas):
+  current_dir=Path(__file__).parent
+  with open(os.path.join(current_dir,'common_words_in_jb_description.txt'),'r') as file:
+    content=file.read()
+  common_words=[]
+  [common_words.append(words) for row in content.split('\n') for words in row.split(',')]
+  return [lemma for lemma in lemmas if lemma not in common_words]
+
 def lemmatize_text(text):
   lemmatizer=WordNetLemmatizer()
   tokenized_text=word_tokenize(text)
@@ -15,7 +25,6 @@ def lemmatize_text(text):
       if pos_tag in ['a','n','r']:
         lemma=lemmatizer.lemmatize(word)
       else:
-        print(lemma,pos_tag,'\n')
         lemma=word
       lemma_list.append(lemma)
   lemma_list=[lemma for lemma in lemma_list if lemma not in ['.',',',')','(','!','-']]
@@ -26,4 +35,8 @@ def lemmatize_text(text):
   for i in range(0,len(lemma_list_lower)):
     if(lemma_list_lower[i] not in lemma_list_no_duplicate[0:len(lemma_list_no_duplicate)+1]):
       lemma_list_no_duplicate.append(lemma_list_lower[i])
-  return list(set(lemma_list_no_duplicate))
+  final_list=remove_common_words(lemma_list_no_duplicate)
+  return list(set(final_list))
+
+if __name__=="__main__":
+  remove_common_words(['sajan','shresth'])
