01fc4316f56b9e229e82e1338b6591ae5a13d02d
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-06-10 20:39:41 +0545

    solved the drag and drop for resume section

--- static/drag_drop_resumes.js
+++ static/drag_drop_resumes.js
@@ -1,29 +1,47 @@
 let container2=document.getElementById('resume-file-upload')
 container2.addEventListener(
-    "dragenter",
-    (e)=>{
-        e.preventDefault();
-        e.stopPropagation();
-        container.classList.add('active')
-    }
-)
-
-container2.addEventListener(
-    "dragleave",
-    (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      container2.classList.remove('active')
-    },
-    false
-  );
-
-  container2.addEventListener(
-    "dragover",
-    (e) => {
+  "dragenter",
+  (e)=>{
       e.preventDefault();
       e.stopPropagation();
       container2.classList.add('active')
-    },
-    false
-  );
+  }
+)
+
+container2.addEventListener(
+  "dragleave",
+  (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    container2.classList.remove("active")
+  },
+  false
+);
+
+container2.addEventListener(
+  "dragover",
+  (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    container2.classList.add("active")
+  },
+  false
+);
+
+container2.addEventListener(
+  "drop",
+  (e)=>{
+      e.preventDefault();
+      e.stopPropagation();
+      container2.classList.remove("active")
+      let dragged_data=e.dataTransfer
+      let files=dragged_data.files
+      console.log(files)
+      const dataTransfer=new DataTransfer();
+      for(i=0;i<files.length;i++){
+        dataTransfer.items.add(files[i])
+      }
+      document.getElementById('resume-content-file').files=dataTransfer.files;
+      uploadResume();
+}
+)
